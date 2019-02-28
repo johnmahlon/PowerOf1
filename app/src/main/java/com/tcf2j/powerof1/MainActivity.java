cd0b834c49a61c502461107796efda6d257b669f
tcf2j <tcf2j@mtmail.mtsu.edu>
2019-02-27 18:34:46 -0600

    Added menu

--- app/src/main/java/com/tcf2j/powerof1/MainActivity.java
+++ app/src/main/java/com/tcf2j/powerof1/MainActivity.java
@@ -1,13 +1,63 @@
 package com.tcf2j.powerof1;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //makes the toolbar the appbar and selects drawer icon
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black);
+
+        //handling when items are clicked in drawer
+        drawerLayout= findViewById(R.id.drawer_layout);
+        NavigationView navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        //item keeps highlight when clicked
+                        menuItem.setChecked(true);
+                        //close drawer when item is selected
+                        drawerLayout.closeDrawers();
+
+
+
+
+                        return true;
+                    }
+                }
+
+        );
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+
 }
